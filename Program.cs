1dbb07f071e5547e561090cfb884c50bfd2ff85d
Daniel Vygolov <rattack@gmail.com>
2019-07-04 22:30:57 +0400

    обработка непредвиденных ошибок

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using RestSharp;
 using System;
 
 namespace FB.BanChecker
@@ -8,6 +7,7 @@ namespace FB.BanChecker
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
@@ -19,14 +19,19 @@ namespace FB.BanChecker
                 Console.WriteLine("Не указан access_token!");
                 return;
             }
-            if(config.GetValue<bool>("check_domains"))
+            if (config.GetValue<bool>("check_domains"))
                 DomainsChecker.Check(apiAddress, accessToken);
-            if(config.GetValue<bool>("check_freeze"))
+            if (config.GetValue<bool>("check_freeze"))
                 FreezeChecker.Check(apiAddress, accessToken);
-            if(config.GetValue<bool>("check_ads"))
-                AdsChecker.Check(apiAddress,accessToken);
+            if (config.GetValue<bool>("check_ads"))
+                AdsChecker.Check(apiAddress, accessToken);
             if (config.GetValue<bool>("check_pages"))
                 PagesChecker.Check(apiAddress, accessToken);
+        }
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Произошла непредвиденная ошибка:"+e.ExceptionObject);
         }
     }
 }
